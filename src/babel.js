933e1fbe6d96c86e25ae6a9e000581022e948b04
steelone <slavikdnsnk@gmail.com>
2021-05-02 13:54:30 +0300

    import lodash as an example

--- src/babel.js
+++ src/babel.js
@@ -11,3 +11,7 @@ class Util {
 console.log("Util Id:", Util.id);
 
 const unused = 42
+import('lodash').then(_ => {
+    console.log('Lodash', _.random(0, 42, true));
+})
+console.log("--------unused:", unused);
